692a6caa18fe84c4bdc14b49a47ad4765a9079c4
Alexandr <35428004+Rawflow@users.noreply.github.com>
2019-11-29 08:19:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,739 @@
+let masTochki19 = [
+   /* {
+        x : 421,
+        y : 406
+    },*/
+    {
+        x : 302,
+        y : 383
+    },
+    {
+        x : 242,
+        y : 495
+    },
+    {
+        x : 143,
+        y : 436
+    },
+    {
+        x : 161,
+        y : 336
+    },
+    {
+        x : 324,
+        y : 255
+    }
+];
+let masTochki29 = [
+    {
+        x : 266,
+        y : 57
+    },
+    {
+        x : 300,
+        y : 49
+    },
+    {
+        x : 408,
+        y : 174
+    },
+    {
+        x : 424,
+        y : 219
+    },
+    {
+        x : 435,
+        y : 240
+    }
+];
+let masTochki39 = [
+    {
+        x : 499,
+        y : 134
+    },
+    {
+        x : 658,
+        y : 35
+    },
+    {
+        x : 653,
+        y : 161
+    },
+    {
+        x : 635,
+        y : 76
+    },
+    {
+        x : 593,
+        y : 185
+    }
+];
+let masTochki49 = [
+    {
+        x : 562,
+        y : 267
+    },
+    {
+        x : 591,
+        y : 272
+    },
+    {
+        x : 548,
+        y : 230
+    },
+    {
+        x : 610,
+        y : 270
+    },
+    {
+        x : 610,
+        y : 257
+    }
+];
+let masTochki59 = [
+    {
+        x : 646,
+        y : 224
+    },
+    {
+        x : 757,
+        y : 144
+    },
+    {
+        x : 777,
+        y : 260
+    },
+    {
+        x : 712,
+        y : 198
+    },
+    {
+        x : 680,
+        y : 276
+    }
+];
+let masTochki69 = [
+    {
+        x : 813,
+        y : 315
+    },
+    {
+        x : 674,
+        y : 240
+    },
+    {
+        x : 729,
+        y : 371
+    },
+    {
+        x : 881,
+        y : 204
+    },
+    {
+        x : 885,
+        y : 87
+    }
+];
+let masTochki79 = [
+    {
+        x : 864,
+        y : 28
+    },
+    {
+        x : 864,
+        y : 161
+    },
+    {
+        x : 775,
+        y : 267
+    },
+    {
+        x : 758,
+        y : 344
+    },
+    {
+        x : 792,
+        y : 425
+    }
+];
+let masTochki89= [
+    {
+        x : 805,
+        y : 455
+    },
+    {
+        x : 814,
+        y : 487
+    },
+    {
+        x : 832,
+        y : 493
+    },
+    {
+        x : 874,
+        y : 523
+    },
+    {
+        x : 819,
+        y : 464
+    }
+];
+let masTochki99= [
+    {
+        x : 718,
+        y : 347
+    },
+    {
+        x : 737,
+        y : 314
+    },
+    {
+        x : 764,
+        y : 365
+    },
+    {
+        x : 726,
+        y : 329
+    },
+    {
+        x : 687,
+        y : 363
+    }
+];
+let masTochki109 = [
+    {
+        x : 683,
+        y : 386
+    },
+    {
+        x : 735,
+        y : 367
+    },
+    {
+        x : 755,
+        y : 413
+    },
+    {
+        x : 684,
+        y : 415
+    },
+    {
+        x : 645,
+        y : 377
+    }
+];
+let masTochki119 = [
+    {
+        x : 485,
+        y : 370
+    },
+    {
+        x : 526,
+        y : 456
+    },
+    {
+        x : 491,
+        y : 290
+    },
+    {
+        x : 564,
+        y : 503
+    },
+    {
+        x : 569,
+        y : 529
+    }
+];
+let masTochki129 = [
+    {
+        x : 569,
+        y : 549
+    },
+    {
+        x : 523,
+        y : 523
+    },
+    {
+        x : 492,
+        y : 493
+    },
+    {
+        x : 438,
+        y : 443
+    },
+    {
+        x : 421,
+        y : 406
+    }
+];
+let masTochkii = [];
+masTochkii.push(masTochki19);
+masTochkii.push(masTochki29);
+masTochkii.push(masTochki39);
+masTochkii.push(masTochki49);
+masTochkii.push(masTochki59);
+masTochkii.push(masTochki69);
+masTochkii.push(masTochki79);
+masTochkii.push(masTochki89);
+masTochkii.push(masTochki99);
+masTochkii.push(masTochki109);
+masTochkii.push(masTochki119);
+masTochkii.push(masTochki129);
+let masTochki1 = [
+    /*{
+        x : 421,
+        y : 406
+    },*/
+    {
+        x : 295,
+        y : 402
+    },
+    {
+        x : 130,
+        y : 401
+    },
+    {
+        x : 145,
+        y : 284
+    },
+    {
+        x : 283,
+        y : 260
+    },
+    {
+        x : 343,
+        y : 251
+    }
+];
+let masTochki2 = [
+    {
+        x : 266,
+        y : 57
+    },
+    {
+        x : 300,
+        y : 49
+    },
+    {
+        x : 408,
+        y : 174
+    },
+    {
+        x : 424,
+        y : 219
+    },
+    {
+        x : 435,
+        y : 240
+    }
+];
+let masTochki3 = [
+    {
+        x : 499,
+        y : 134
+    },
+    {
+        x : 658,
+        y : 35
+    },
+    {
+        x : 653,
+        y : 161
+    },
+    {
+        x : 635,
+        y : 76
+    },
+    {
+        x : 593,
+        y : 185
+    }
+];
+let masTochki4 = [
+    {
+        x : 562,
+        y : 267
+    },
+    {
+        x : 591,
+        y : 272
+    },
+    {
+        x : 548,
+        y : 230
+    },
+    {
+        x : 610,
+        y : 270
+    },
+    {
+        x : 610,
+        y : 257
+    }
+];
+let masTochki5 = [
+    {
+        x : 632,
+        y : 226
+    },
+    {
+        x : 719,
+        y : 100
+    },
+    {
+        x : 723,
+        y : 247
+    },
+    {
+        x : 685,
+        y : 164
+    },
+    {
+        x : 680,
+        y : 276
+    }
+];
+let masTochki6 = [
+    {
+        x : 834,
+        y : 320
+    },
+    {
+        x : 674,
+        y : 240
+    },
+    {
+        x : 729,
+        y : 371
+    },
+    {
+        x : 917,
+        y : 201
+    },
+    {
+        x : 930,
+        y : 95
+    }
+];
+let masTochki7 = [
+    {
+        x : 948,
+        y : 45
+    },
+    {
+        x : 955,
+        y : 169
+    },
+    {
+        x : 892,
+        y : 268
+    },
+    {
+        x : 836,
+        y : 333
+    },
+    {
+        x : 874,
+        y : 413
+    }
+];
+let masTochki8 = [
+    {
+        x : 903,
+        y : 457
+    },
+    {
+        x : 926,
+        y : 512
+    },
+    {
+        x : 973,
+        y : 448
+    },
+    {
+        x : 957,
+        y : 542
+    },
+    {
+        x : 842,
+        y : 456
+    }
+];
+let masTochki9 = [
+    {
+        x : 718,
+        y : 347
+    },
+    {
+        x : 737,
+        y : 314
+    },
+    {
+        x : 764,
+        y : 365
+    },
+    {
+        x : 726,
+        y : 329
+    },
+    {
+        x : 685,
+        y : 360
+    }
+];
+let masTochki10 = [
+    {
+        x : 673,
+        y : 397
+    },
+    {
+        x : 737,
+        y : 412
+    },
+    {
+        x : 676,
+        y : 424
+    },
+    {
+        x : 661,
+        y : 404
+    },
+    {
+        x : 645,
+        y : 377
+    }
+];
+let masTochki11 = [
+    {
+        x : 485,
+        y : 370
+    },
+    {
+        x : 526,
+        y : 456
+    },
+    {
+        x : 491,
+        y : 290
+    },
+    {
+        x : 564,
+        y : 503
+    },
+    {
+        x : 569,
+        y : 529
+    }
+];
+let masTochki12 = [
+    {
+        x : 569,
+        y : 549
+    },
+    {
+        x : 523,
+        y : 523
+    },
+    {
+        x : 492,
+        y : 493
+    },
+    {
+        x : 438,
+        y : 443
+    },
+    {
+        x : 421,
+        y : 406
+    }
+];
+let masTochki = [];
+masTochki.push(masTochki1);
+masTochki.push(masTochki2);
+masTochki.push(masTochki3);
+masTochki.push(masTochki4);
+masTochki.push(masTochki5);
+masTochki.push(masTochki6);
+masTochki.push(masTochki7);
+masTochki.push(masTochki8);
+masTochki.push(masTochki9);
+masTochki.push(masTochki10);
+masTochki.push(masTochki11);
+masTochki.push(masTochki12);
+window.onload = function (){
+    let c = document.getElementById("myCanvas");
+    let setka = this.document.getElementById('sitka');
+    setka.addEventListener('change',function(e){
+        if(setka.checked){
+            main(true);
+        }else{
+            main(false);
+        }
+    });
+    let button = this.document.getElementById('button');
+    button.addEventListener('click', main);
+    let button1 = this.document.getElementById('button1');
+    button1.addEventListener('click', animate);
+    
+    c.addEventListener('mousedown', (e) => {
+        for (let value=0; value < masTochki.length; value++){
+            for(let i=0; i < masTochki[value].length ; i++){
+                if( (e.clientX >= masTochki[value][i].x - 5 && e.clientX <= masTochki[value][i].x + 5) && (e.clientY >= masTochki[value][i].y - 5 && e.clientY <= masTochki[value][i].y + 5) ){
+                    c.addEventListener('mousemove', sanya = (e) => {
+                        if(i === masTochki[value].length-2){
+                            if( value === masTochki.length-1) hladko(masTochki[value][i],masTochki[value][i+1],masTochki[0][0],e);
+                            else hladko(masTochki[value][i],masTochki[value][i+1],masTochki[value+1][0],e);
+                        }else {
+                            move(masTochki[value][i],e);
+                        }
+
+                    });
+                    c.addEventListener('mouseup', ()=>{
+                        c.removeEventListener('mousemove', sanya);
+                    });
+                }
+            }
+        }
+        
+    })
+
+    function hladko(obj1,obj2,obj3,e){
+        obj1.x = e.clientX;
+        obj1.y = e.clientY;
+        obj3.x = obj2.x + (obj2.x - obj1.x);
+        obj3.y = obj2.y + (obj2.y - obj1.y);
+        drawDetail(true);
+        drawSitka();
+    }
+
+    function move(obj,e){
+        obj.x = e.clientX;
+        obj.y = e.clientY;
+        drawDetail(true);
+        drawSitka();
+    }
+
+    function drawSitka(){
+        let c = document.getElementById("myCanvas");
+        let ctx=c.getContext('2d');
+        ctx.beginPath();
+        ctx.setLineDash([]);
+        ctx.strokeStyle="#FF45FF"
+        for(let i=0; i<1000; i+=50){
+            ctx.moveTo(i,0);
+            ctx.fillText(i, i+1, 13);
+            ctx.lineTo(i,600); }
+        for(let i=0; i<600; i+=50){
+            ctx.moveTo(0,i);
+            ctx.fillText(i, 1, i+13);
+            ctx.lineTo(1000,i); }
+        ctx.moveTo(50,50);
+        ctx.fillText('X', 988, 13);
+        ctx.fillText('Y', 1, 598);
+        ctx.stroke();      
+    }   
+
+    function animate(){
+        console.log(masTochki);
+        for(let k=0;k<800;k++){
+            let jj = setTimeout(()=>{
+                for(let i =0; i < masTochki.length-2;i++){
+                    for(let j=0; j < masTochki[i].length;j++){
+                        masTochki[i][j].x =  masTochki[i][j].x - (masTochki[i][j].x - masTochkii[i][j].x)/300;
+                        masTochki[i][j].y =  masTochki[i][j].y - (masTochki[i][j].y - masTochkii[i][j].y)/300;
+                    }
+                }
+                drawDetail(false);
+            },1);
+            
+        }
+    }
+
+    function drawDetail(bool){
+        var c = document.getElementById("myCanvas");
+        var ctx=c.getContext('2d');
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.beginPath();
+        ctx.setLineDash([]);
+        ctx.strokeStyle="#000000";
+        let x;
+        let y;
+        let masBezie = [];
+        for(let i = 0; i < masTochki.length; i++){
+            if(i === 0){
+                for(let t = 0; t <= 1; t+=0.01){
+                    x = Math.pow((1-t),5)*masTochki[masTochki.length - 1][4].x + 5*t*Math.pow((1-t), 4)*masTochki[i][0].x + 10*t*t*Math.pow((1-t),3)*masTochki[i][1].x + 10*Math.pow(t,3)*Math.pow((1-t),2)*masTochki[i][2].x + 5*Math.pow(t,4)*(1-t)*masTochki[i][3].x + Math.pow(t,5)*masTochki[i][4].x;
+                    y = Math.pow((1-t),5)*masTochki[masTochki.length - 1][4].y + 5*t*Math.pow((1-t), 4)*masTochki[i][0].y + 10*t*t*Math.pow((1-t),3)*masTochki[i][1].y + 10*Math.pow(t,3)*Math.pow((1-t),2)*masTochki[i][2].y + 5*Math.pow(t,4)*(1-t)*masTochki[i][3].y + Math.pow(t,5)*masTochki[i][4].y;
+                    masBezie.push({
+                        x : x,
+                        y : y
+                    });
+                }
+            }else{
+                for(let t = 0; t <= 1; t+=0.01){
+                    x = Math.pow((1-t),5)*masTochki[i-1][masTochki[i-1].length-1].x + 5*t*Math.pow((1-t), 4)*masTochki[i][0].x + 10*t*t*Math.pow((1-t),3)*masTochki[i][1].x + 10*Math.pow(t,3)*Math.pow((1-t),2)*masTochki[i][2].x + 5*Math.pow(t,4)*(1-t)*masTochki[i][3].x + Math.pow(t,5)*masTochki[i][4].x;
+                    y = Math.pow((1-t),5)*masTochki[i-1][masTochki[i-1].length-1].y + 5*t*Math.pow((1-t), 4)*masTochki[i][0].y + 10*t*t*Math.pow((1-t),3)*masTochki[i][1].y + 10*Math.pow(t,3)*Math.pow((1-t),2)*masTochki[i][2].y + 5*Math.pow(t,4)*(1-t)*masTochki[i][3].y + Math.pow(t,5)*masTochki[i][4].y;
+                    masBezie.push({
+                        x : x,
+                        y : y
+                    });
+                }
+            }
+        }
+        draw(masBezie);
+        if(bool) draw1(masTochki);
+    }
+
+    function draw(mas){
+        var c = document.getElementById("myCanvas");
+        var ctx=c.getContext('2d');
+        ctx.beginPath();
+        ctx.setLineDash([]);
+        ctx.lineWidth = 3;
+        ctx.strokeStyle="#000000";
+        ctx.moveTo(mas[0].x,mas[0].y);
+        for (let value of mas){
+            ctx.lineTo(value.x, value.y);
+        }
+        ctx.lineTo(mas[0].x,mas[0].y);
+        ctx.stroke();
+        ctx.lineWidth = 1;
+    }
+
+    function draw1(mas){
+        var c = document.getElementById("myCanvas");
+        var ctx=c.getContext('2d');
+        ctx.beginPath();
+        ctx.setLineDash([]);
+        ctx.strokeStyle="#000000";
+        ctx.moveTo(mas[0][0].x,mas[0][0].y);
+        for (let value of mas){
+            for(let value1 of value){
+                ctx.lineTo(value1.x, value1.y);
+            }
+        }
+        for (let value of mas){
+            for(let value1 of value){
+                ctx.moveTo(value1.x, value1.y);
+                for(let i= 0; i < 360; i += 20) {
+                    angle = i;
+                    let xn = 5 * Math.cos(angle * Math.PI/180);
+                    let yn = 5 * Math.sin(angle * Math.PI/180);
+                    ctx.lineTo(value1.x + xn, value1.y + yn);
+                }
+                ctx.moveTo(value1.x, value1.y );
+            }
+        }
+        ctx.lineTo(mas[0][0].x,mas[0][0].y);
+        ctx.stroke();
+    }
+
+    function main(sitka){
+        let c = document.getElementById("myCanvas");
+        let ctx  =c.getContext('2d');
+        ctx.clearRect(0, 0, c.width, c.height);
+        drawDetail(sitka);
+        if(sitka) drawSitka();
+    }
+
+}
